242dbd1c81f5a6c9a9fb0bcab33b1d2461814996
Vadla Vinod <138966638+vinodvadla@users.noreply.github.com>
2025-02-05 23:53:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static("public"));
 
 // Define a simple route to handle a GET request
 app.get("/", (req, res) => {
-  res.send("Hello, World! cicd testing");
+  res.send("Hello, World! cicd testing1");
 });
 
 // Define another route for a different path
